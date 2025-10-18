709c8831ead9e8b3ec242182afa74aded2c22471
Денис <denyar3100@gmail.com>
2025-10-19 00:42:29 +0400

    java translated 1.0 ver complete

--- src/main/java/ru/kaed/Main.java
+++ src/main/java/ru/kaed/Main.java
@@ -1,7 +1,10 @@
 package ru.kaed;
 
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args) {
+        System.setOut(new java.io.PrintStream(System.out, true, StandardCharsets.UTF_8));
         Game.start();
     }
 }
